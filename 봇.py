dc0b598c467a09033f6e0d70c2d406c348056973
garde1540 <67937699+garde1540@users.noreply.github.com>
2020-07-07 11:49:10 +0900

    Update 봇.py

--- 봇.py
+++ 봇.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Client()
 
@@ -10,4 +11,6 @@ async def on_message(message):
         msg = message.content[5:]
         await client .get_channel(int(690199478401237072)).send(msg)
 
+        
+        
 client.run("NzI5NTUxOTMwMTM2ODU0NTU4.XwLHUQ.41GNyu2hAN159h5L_T9dW7rH1Mk")
